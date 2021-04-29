d1e4afdd39e37411e84006cbeb1e27dd43c0752f
wyl_pi <wyl1346788525@163.com>
2021-04-29 20:59:23 +0800

    error opencv-contrib

--- IntelligenceHomeSys/SecuritySubsystem/基于OpenCV的人脸识别/faceDetection.py
+++ IntelligenceHomeSys/SecuritySubsystem/基于OpenCV的人脸识别/faceDetection.py
@@ -12,8 +12,8 @@ import cv2
 faceCascade = cv2.CascadeClassifier('Cascades/haarcascade_frontalface_default.xml')
 
 cap = cv2.VideoCapture(0)
-cap.set(3,320) # set Width
-cap.set(4,240) # set Height
+cap.set(3,640) # set Width
+cap.set(4,480) # set Height
 
 while True:
     ret, img = cap.read()
